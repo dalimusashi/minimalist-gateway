0c1755ce343fde2a951cdca2766716e265f0063d
Dali Musashi <dalimusashi@gmail.com>
2026-02-05 19:35:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5485530333-69524",
+  "appId": "1:1090291723757:web:cd653a331a051f4bb18be4",
+  "apiKey": "AIzaSyCIXMKatWXMpfnwsT859gWWswHzZA8UOrM",
+  "authDomain": "studio-5485530333-69524.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1090291723757"
+};
