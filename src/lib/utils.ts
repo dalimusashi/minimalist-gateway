4e71a0fd01d4808fb0885ad9de7b157fe86a61b3
dalimusashi <dalimusashi@gmail.com>
2026-02-06 02:04:33 +0000

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,1 +1,8 @@
-
+// src/lib/utils.ts
+export function cn(...classes: Array<string | false | null | undefined>) {
+  return classes.filter(Boolean).join(" ");
+}
+// src/lib/utils.ts
+export function cn(...classes: Array<string | false | null | undefined>) {
+  return classes.filter(Boolean).join(" ");
+}
